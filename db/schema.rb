51e381db564ef2d1122c40aec4466fb23758e0ad
spatle <sandeep.patle@heyjobs.de>
2021-09-28 16:06:29 +0200

    workling version

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_081305) do
+ActiveRecord::Schema.define(version: 2021_09_23_081154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cats", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "rats", force: :cascade do |t|
+  create_table "dogs", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
